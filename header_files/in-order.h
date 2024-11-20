ecfca0847c964a3d1b4ad54a5efc062cd6cb50c2
Isidoros Kalamaris <isikala0@gmail.com>
2024-11-20 18:51:14 +0200

    ordinal pi approximation implemented

---
+++ header_files/in-order.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+#pragma once
+
+double randfrom(double min, double max);
+void ordinal_Pi_calculation(const long int number_of_throws){
---
+++ modules/assignment1.c
@@ -0,0 +1,28 @@
+#include "in-order.h"
+
+//function to generate a  random double number from the range of max-min(1-(-1))
+double randfrom(double min, double max) 
+{
+    double range = (max - min); 
+    double div = RAND_MAX / range;
+    return min + (rand() / div);
+}
+
+
+void ordinal_Pi_calculation(const long int number_of_throws){
+    long int cicrle_arrows=0;
+    for(long int i=0;i<number_of_throws;i++){
+        double x = randfrom(1.0,-1.0);
+        double y = randfrom(1.0,-1.0);
+        double squared_distance = x*x + y*y;
+        if(squared_distance <= 1)
+            cicrle_arrows++;
+    }
+    double Pi_approximation=4*cicrle_arrows/((double)number_of_throws);
+    printf("pi is %f \n",Pi_approximation);
+}
+
+int main(){
+    long int number_of_throws=1000000;
+    Pi_calculation(number_of_throws);
+}
